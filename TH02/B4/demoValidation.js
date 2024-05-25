f3e554012c84e459949785febd115951bfcccb75
shinkawar <ngguuyyenthang@gmail.com>
2024-05-25 16:38:54 +0700

    BT TH02, B123 with jquery miss, B4 with jquery full

--- TH02/B4/demoValidation.js
+++ TH02/B4/demoValidation.js
@@ -1,5 +1,5 @@
 $().ready(function() {
-	 $.validator.addMethod("validatePassword", function (value, element) {
+	 $.validator.addMethod("empid", function (value, element) {
 	 	return this.optional(element) || /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$/i.test(value);
 	 }, "Hãy nhập password từ 8 đến 16 ký tự bao gồm chữ hoa, chữ thường và ít nhất một chữ số");
 	$("#demoForm").validate({
